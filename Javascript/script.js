0f69b8988e803a07151ba556bd779b9cbc7d63a8
Parth63-ai <kumarpartha726@gmail.com>
2026-02-03 20:19:10 +0530

    Manage callback hell

--- Javascript/script.js
+++ Javascript/script.js
@@ -221,3 +221,31 @@ setTimeout(() => {
         }, 1000); // after dinner
     }, 1500); // dinner time
 }, 2000); // homework time
+
+
+function finishHomework(callback) {
+    console.log("Starting homework...");
+    setTimeout(() => {
+        console.log("Homework done!");
+        callback();
+    }, 2000);
+}
+
+function eatDinner(callback) {
+    console.log("Starting dinner...");
+    setTimeout(() => {
+        console.log("Dinner done!");
+        callback();
+    }, 1500);
+}
+
+function goToPlayground() {
+    console.log("Going to the playground!");
+}
+
+// Chained in steps, but cleaner
+finishHomework(() => {
+    eatDinner(() => {
+        goToPlayground();
+    });
+});
